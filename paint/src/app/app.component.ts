f05760af5a90db1f86bb24f57dbb7ecfa36b4be3
NancyHisham <96303049+NancyHisham@users.noreply.github.com>
2022-12-02 00:52:58 +0200

    first raw

--- paint/src/app/app.component.ts
+++ paint/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { UntypedFormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
   title = 'paint';
+  redo(){console.log("redo")}
+  undo(){console.log("undo")}
+  
 }
