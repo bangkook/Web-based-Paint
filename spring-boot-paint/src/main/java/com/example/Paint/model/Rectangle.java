dcfad9cc3d910e987d301afa2c2a11a8abe33271
Rowaina <rowainaabdelnasser@gmail.com>
2022-12-05 00:17:44 +0200

    some parameters changed

--- spring-boot-paint/src/main/java/com/example/Paint/model/Rectangle.java
+++ spring-boot-paint/src/main/java/com/example/Paint/model/Rectangle.java
@@ -1,28 +1,22 @@
 package com.example.Paint.model;
 
 public class Rectangle extends Shape {
-    private float length;
+    private float height;
     private float width;
 
-    public Rectangle(float x, float y, float length, float width) {
+    public Rectangle(float x, float y, float width, float height) {
         startPoint = new Point(x, y);
-        this.length = length;
+        this.height = height;
         this.width = width;
         this.type = ShapeType.RECTANGLE;
     }
 
-    /*public Rectangle(float x1, float y1, float x2, float y2) {
-        startPoint = new Point(x, y);
-        this.length = Math.abs(x1 - x2);
-        this.width = Math.abs(y1 - y2);
-    }*/
-
-    public float getLength() {
-        return length;
+    public float getHeight() {
+        return height;
     }
 
-    public void setLength(float length) {
-        this.length = length;
+    public void setHeight(float height) {
+        this.height = height;
     }
 
     public float getWidth() {
--- spring-boot-paint/src/main/java/com/example/Paint/model/Triangle.java
+++ spring-boot-paint/src/main/java/com/example/Paint/model/Triangle.java
@@ -1,28 +1,26 @@
 package com.example.Paint.model;
 
+//Triangle is represented using 3 points --> if coordinates better to be used, convert them to x1, y1, x2, y2, x3, y3--> to be represented as polygon in thr front-end
 public class Triangle extends Shape {
-    float width, height;
+    Point p1, p2, p3;
 
-    public Triangle(float x, float y, float width, float height) {
-        this.startPoint = new Point(x, y);
-        this.width = width;
-        this.height = height;
+    public Triangle(Point p1, Point p2, Point p3) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
         this.type = ShapeType.TRIANGLE;
     }
 
-    public float getWidth() {
-        return width;
-    }
+    public void setP1(Point p1) { this.p1 = p1; }
 
-    public void setWidth(float width) {
-        this.width = width;
-    }
+    public void setP2(Point p1) { this.p2 = p2; }
 
-    public float getHeight() {
-        return height;
-    }
+    public void setP3(Point p1) { this.p3 = p3; }
 
-    public void setHeight(float height) {
-        this.height = height;
-    }
+    public Point getP1() { return this.p1; }
+
+    public Point getP2() { return this.p2; }
+
+    public Point getP3() { return this.p3; }
+
 }
